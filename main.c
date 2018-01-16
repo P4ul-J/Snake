6c96a1ef86bcf5be2fc49bb2a8ad5e654ceba5f5
Lihring <Nick.Lihring@draeger.com>
2018-01-16 02:01:05 +0100

    paar Spielereien :) Objekt kann mit Pfeiltasten im Fenster bewegt werden

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
 
-
+#define WINDOW_WIDTH (640)
+#define WINDOW_HEIGHT (480)
+// Geschwindigkeit in Pixel pro Sekunde
+#define SPEED (300)
 
 int main(int argc, char* argv[]) {
 
@@ -16,7 +19,7 @@ if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
 printf("Initialiserung erfolgreich!\n");
 
 //Fenster erstellen
-SDL_Window *win = SDL_CreateWindow("Fenster", 100, 100, 640, 480, 0);
+SDL_Window *win = SDL_CreateWindow("Snake", 100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
 
 if (win == NULL) {
   printf("Fenster konnte nicht erstellt werden: %s\n", SDL_GetError());
@@ -56,21 +59,127 @@ if (tex == NULL) {
   return 1;
 }
 
-// Fenster "säubern" -> Schwarzer Hintergrund
+// Struct für Größe und Position des Sprites
+SDL_Rect dest;
+
+// Dimensionen der Textur wird in struct gespeichert
+SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
+
+// Textur Skalieren
+dest.w /= 4;
+dest.h /= 4;
+
+// Sprite in der Mitte platzieren
+// Koordinatenursprung ist oben links, positive y-Achse zeigt nach unten
+dest.x = (WINDOW_WIDTH - dest.w) / 2;
+dest.y = (WINDOW_HEIGHT - dest.h) / 2;
+
+int up = 0;
+int down = 0;
+int left = 0;
+int right = 0;
+
+//Bewegungsrichtung
+float x_vel = 0;
+float y_vel = 0;
+
+int exitbutton = 0;
+
+// gameloop
+while (exitbutton == 0)
+    {
+        // process events
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                exitbutton = 1;
+                break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.scancode)
+                {
+                case SDL_SCANCODE_W:
+                case SDL_SCANCODE_UP:
+                    up = 1;
+                    down = left = right = 0;
+                    break;
+                case SDL_SCANCODE_A:
+                case SDL_SCANCODE_LEFT:
+                    left = 1;
+                    up = down = right = 0;
+                    break;
+                case SDL_SCANCODE_S:
+                case SDL_SCANCODE_DOWN:
+                    down = 1;
+                    up = right = left = 0;
+                    break;
+                case SDL_SCANCODE_D:
+                case SDL_SCANCODE_RIGHT:
+                    right = 1;
+                    up = down = left = 0;
+                    break;
+                }
+                break;
+             case SDL_KEYUP:
+                /*switch (event.key.keysym.scancode)
+                {
+                case SDL_SCANCODE_W:
+                case SDL_SCANCODE_UP:
+                    up = 0;
+                    break;
+                case SDL_SCANCODE_A:
+                case SDL_SCANCODE_LEFT:
+                    left = 0;
+                    break;
+                case SDL_SCANCODE_S:
+                case SDL_SCANCODE_DOWN:
+                    down = 0;
+                    break;
+                case SDL_SCANCODE_D:
+                case SDL_SCANCODE_RIGHT:
+                    right = 0;
+                    break;
+                }  */
+                break;
+            }
+}
+
+// determine velocity
+x_vel = y_vel = 0;
+if (up && !down) y_vel = -SPEED;
+if (down && !up) y_vel = SPEED;
+if (left && !right) x_vel = -SPEED;
+if (right && !left) x_vel = SPEED;
+
+// update positions
+dest.x += x_vel / 60;
+dest.y += y_vel / 60;
+
+// collision detection with bounds
+if (dest.x <= 0) dest.x = 0;
+if (dest.y <= 0) dest.y = 0;
+if (dest.x >= WINDOW_WIDTH - dest.w) dest.x = WINDOW_WIDTH - dest.w;
+if (dest.y >= WINDOW_HEIGHT - dest.h) dest.y = WINDOW_HEIGHT - dest.h;
+
+// clear the window
 SDL_RenderClear(rend);
 
-// Bild in das Fenster zeichnen
-SDL_RenderCopy(rend, tex, NULL, NULL); // Double Buffering
+// draw the image to the window
+SDL_RenderCopy(rend, tex, NULL, &dest);
 SDL_RenderPresent(rend);
 
-// 5 Sekunden warten
-SDL_Delay(5000);
+// wait 1/60th of a second
+SDL_Delay(1000/60);
 
-// Resourcen vorm Beenden befreien
+}
+
+// cleanup
 SDL_DestroyTexture(tex);
 SDL_DestroyRenderer(rend);
 SDL_DestroyWindow(win);
-
 SDL_Quit();
+
 return 0;
 }
